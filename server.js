cf5181998288576f85e111cba5a99b8507c4982c
Nicholai Venetucci <npv2@pdx.edu>
2020-05-22 13:03:34 -0700

    add express starter skeleton

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// this is the steamburst backend
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
