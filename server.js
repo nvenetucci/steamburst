6b00b36ef5e858889b492df137dde7eec4c59836
Nicholai Venetucci <npv2@pdx.edu>
2020-05-23 05:07:18 -0700

    adjust endpoint to fetch top 100 most-played

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const fetch = require("node-fetch");
+const cheerio = require("cheerio");
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  fetch("https://store.steampowered.com/stats/")
+    .then((res) => res.text())
+    .then((html) => {
+      const $ = cheerio.load(html);
+
+      const data = [];
+
+      data.push({ updated: $(".statsTopSmall").text() });
+
+      $(".player_count_row").each((i, elem) => {
+        const current = $(elem).find(".currentServers").first().text();
+        const peak = $(elem).find(".currentServers").last().text();
+        const game = $(elem).find(".gameLink").text();
+
+        const obj = {
+          current,
+          peak,
+          game,
+        };
+
+        data.push(obj);
+      });
+
+      res.json(data);
+    });
 });
 
 app.get("/countries", (req, res) => {
