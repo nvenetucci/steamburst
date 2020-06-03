bd70184dc07369523a51adf195d6b70d4231185c
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2020-06-03 03:47:49 -0700

    table for top deals, better deals, some color changes

--- client/src/components/TopDealsTable.js
+++ client/src/components/TopDealsTable.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Table from "react-bootstrap/Table";
+import SearchResultsData from "./SearchResultsData";
 
 class TopDealsTable extends Component {
   checkIfOnSteam(name, shop_url) {
     var temp;
     temp = this.props.getIdByName(name);
-    if(temp == undefined){
-        return <a href={shop_url}>{name}</a>
+    if(temp === undefined){
+        return <a style={{ color: "white" }} href={shop_url}>{name}</a>
     } else {
-        return <Link to={`/app/${temp}`}>{name}</Link>;
+        return <Link style={{ color: "white" }} to={`/app/${temp}`}>{name}</Link>;
     }
   }
 
@@ -17,26 +18,21 @@ class TopDealsTable extends Component {
     return (
       <Table striped bordered variant="dark">
         <thead>
-          <tr>
-            <th style={{ width: "10%" }}>img</th>
-            <th style={{ width: "30%" }}>Name</th>
-            <th style={{ width: "25%" }}>Current Price</th>
-            <th style={{ width: "20%" }}>Previous Price</th>
-            <th style={{ width: "5%" }}>Savings</th>
-            <th style={{ width: "10%" }}>Shop</th>
-          </tr>
         </thead>
         <tbody>
           {this.props.apps.map((app, index) => (
-            <tr key={app.appid}>
-              <td>{`${this.props.indexOfLastApp + index - 9}.`}</td>
-              <td>
-                <Link style={{ color: "white" }} to={`/app/${app.appid}`}>
-                  {app.name}
-                </Link>
+            <tr key={this.props.getIdByName(app.title)}>
+              <td className={"align-middle"} style={{ width: "2%" }}>{`${this.props.indexOfLastApp + index - 9}.`}</td>
+              <td className={"align-middle table-borderless"} style={{ width: "8%" }}><SearchResultsData appid={this.props.getIdByName(app.title)} /></td>
+              <td className={"align-middle"} style={{ width: "15%" }}>
+                {this.checkIfOnSteam(app.title, app.urls.buy)}
               </td>
-              <td>{app.current}</td>
-              <td>{app.peak}</td>
+              <td className={"align-middle font-weight-bold font-italic"} style={{ width: "10%" }}>
+                ${app.price_new}
+              </td>
+              <td className={"align-middle"} style={{ width: "10%" }}>${app.price_old}</td>
+              <td className={"align-middle"} style={{ width: "5%" }}>{app.price_cut}% OFF</td>
+              <td className={"align-middle"} style={{ width: "10%" }}><a href={app.urls.buy}>{app.shop.name}</a></td>
             </tr>
           ))}
         </tbody>
@@ -45,4 +41,4 @@ class TopDealsTable extends Component {
   }
 }
 
-export default SteamTop100Table;
+export default TopDealsTable;
