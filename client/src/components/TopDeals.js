e3ae80dd11482456476a7bfa2659639a3277a12c
Nicholai Venetucci <npv2@pdx.edu>
2020-06-04 15:54:02 -0700

    reformat TopDeals.js with Prettier

--- client/src/components/TopDeals.js
+++ client/src/components/TopDeals.js
@@ -4,79 +4,82 @@ import Pagination from "react-bootstrap/Pagination";
 import TopDealsTable from "./TopDealsTable";
 
 class TopDeals extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoaded: false,
-            deal_data: [],
-            currentPage: 1,
-            appsPerPage: 10,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      deal_data: [],
+      currentPage: 1,
+      appsPerPage: 10,
+    };
+  }
+
+  componentDidMount() {
+    fetch("/deals")
+      .then((res) => res.json())
+      .then((data) => this.setState({ isLoaded: true, deal_data: data }));
+  }
+
+  paginate = (event) => {
+    event.preventDefault();
+
+    if (this.currentPage !== event.target.text) {
+      this.setState({ currentPage: event.target.text });
     }
+  };
 
-    componentDidMount() {
-        fetch("/deals")
-        .then((res) => res.json())
-        .then((data) => this.setState({isLoaded: true, deal_data: data}));
+  checkIfOnSteam(name, shop_url) {
+    var temp;
+    temp = this.props.getIdByName(name);
+    if (temp === undefined) {
+      return <a href={shop_url}>{name}</a>;
+    } else {
+      return <Link to={`/app/${temp}`}>{name}</Link>;
     }
+  }
 
-    paginate = (event) => {
-        event.preventDefault();
-    
-        if (this.currentPage !== event.target.text) {
-          this.setState({ currentPage: event.target.text });
-        }
-      };
+  render() {
+    const { isLoaded, deal_data, currentPage, appsPerPage } = this.state;
 
-    checkIfOnSteam(name, shop_url) {
-        var temp;
-        temp = this.props.getIdByName(name);
-        if(temp === undefined){
-            return <a href={shop_url}>{name}</a>
-        } else {
-            return <Link to={`/app/${temp}`}>{name}</Link>;
-        }
-    }
-
-    render() {
-        
-        const { isLoaded, deal_data, currentPage, appsPerPage } = this.state;
-
-        if (!isLoaded) {
-            return (
-            <div className="text-center">
-              <div className="spinner-border text-light" role="status">
-                <span className="sr-only">Loading...</span>
-              </div>
-            </div>
-            )
-        }
-
-        const indexOfLastApp = currentPage * appsPerPage;
-        const indexOfFirstApp = indexOfLastApp - appsPerPage;
-        const currentApps = deal_data.slice(indexOfFirstApp, indexOfLastApp);
-        const pageNumbers = [];
-
-        for (
-            let number = 1;
-            number <= Math.ceil(deal_data.length / appsPerPage);
-            number++
-          ) {
-            pageNumbers.push(
-              <Pagination.Item key={number} active={number === parseInt(currentPage)}>
-                {number}
-              </Pagination.Item>
-            );
-          }
-
-        return (
-        <div className="TopDeals container mt-5">
-            <h1 className={"display-4 text-left text-white"} >Top Deals</h1>
-            <TopDealsTable apps={currentApps} indexOfLastApp={indexOfLastApp} getIdByName={this.props.getIdByName} />
-            <Pagination onClick={this.paginate}>{pageNumbers}</Pagination>
+    if (!isLoaded) {
+      return (
+        <div className="text-center">
+          <div className="spinner-border text-light" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
         </div>
-        );
+      );
     }
+
+    const indexOfLastApp = currentPage * appsPerPage;
+    const indexOfFirstApp = indexOfLastApp - appsPerPage;
+    const currentApps = deal_data.slice(indexOfFirstApp, indexOfLastApp);
+    const pageNumbers = [];
+
+    for (
+      let number = 1;
+      number <= Math.ceil(deal_data.length / appsPerPage);
+      number++
+    ) {
+      pageNumbers.push(
+        <Pagination.Item key={number} active={number === parseInt(currentPage)}>
+          {number}
+        </Pagination.Item>
+      );
+    }
+
+    return (
+      <div className="TopDeals container mt-5">
+        <h1 className={"display-4 text-left text-white"}>Top Deals</h1>
+        <TopDealsTable
+          apps={currentApps}
+          indexOfLastApp={indexOfLastApp}
+          getIdByName={this.props.getIdByName}
+        />
+        <Pagination onClick={this.paginate}>{pageNumbers}</Pagination>
+      </div>
+    );
+  }
 }
 
 export default TopDeals;
