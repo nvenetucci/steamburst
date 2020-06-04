50bc243fcba61aaa9657e09be6617707a77ffde0
Nicholai Venetucci <npv2@pdx.edu>
2020-06-04 15:27:44 -0700

    fix console warning from the IntroTitle component

--- client/src/components/IntroTitle.js
+++ client/src/components/IntroTitle.js
@@ -5,10 +5,10 @@ class IntroTitle extends Component {
         return (
         <div 
         className={" d-flex justify-content-center"}
-        style={{padding:"3rem", "background-image":"linear-gradient(180deg, rgba(100,100,100,1) 0%, rgba(150,150,150,1) 50%, rgba(170,170,170,0) 100%)"}}>
+        style={{padding:"3rem", "backgroundImage":"linear-gradient(180deg, rgba(100,100,100,1) 0%, rgba(150,150,150,1) 50%, rgba(170,170,170,0) 100%)"}}>
             <div 
             className={"jumbotron text-center text-white bg-transparent"} 
-            style={{width:"75%", "background-image":"linear-gradient(90deg, rgba(23,23,23,0) 0%, rgba(23,23,23,0.7) 15%, rgba(23,23,23,1) 40%, rgba(23,23,23,1) 60%, rgba(23,23,23,0.7) 85%, rgba(23,23,23,0) 100%)"}}>
+            style={{width:"75%", "backgroundImage":"linear-gradient(90deg, rgba(23,23,23,0) 0%, rgba(23,23,23,0.7) 15%, rgba(23,23,23,1) 40%, rgba(23,23,23,1) 60%, rgba(23,23,23,0.7) 85%, rgba(23,23,23,0) 100%)"}}>
                 <h1 className={"display-1"} >Steam Burst</h1>
                 <h3 className={"lead"}>Welcome to Steam Burst, a website dedicated to bringing you all sorts of information about Steam games</h3>
             </div>
