9d0c23d0b1f6eef998df565ffa8ce55d880b0a6b
Nicholai Venetucci <npv2@pdx.edu>
2020-06-04 17:50:37 -0700

    reformat TopDealsTable.js with Prettier

--- client/src/components/TopDealsTable.js
+++ client/src/components/TopDealsTable.js
@@ -7,37 +7,68 @@ class TopDealsTable extends Component {
   checkIfOnSteam(name, shop_url, to_display) {
     var temp;
     temp = this.props.getIdByName(name);
-    if(temp === undefined){
-        return <a style={{ color: "white" }} href={shop_url}>{to_display}</a>
+    if (temp === undefined) {
+      return (
+        <a style={{ color: "white" }} href={shop_url}>
+          {to_display}
+        </a>
+      );
     } else {
-        return <Link style={{ color: "white" }} to={`/app/${temp}`}>{to_display}</Link>;
+      return (
+        <Link style={{ color: "white" }} to={`/app/${temp}`}>
+          {to_display}
+        </Link>
+      );
     }
   }
 
   render() {
     return (
       <Table striped bordered variant="dark" responsive>
-        <thead>
-        </thead>
+        <thead></thead>
         <tbody>
           {this.props.apps.map((app, index) => (
             <tr key={this.props.indexOfLastApp + index - 9}>
-              <td className={"align-middle"} style={{ width: "2%" }}>{`${this.props.indexOfLastApp + index - 9}.`}</td>
+              <td className={"align-middle"} style={{ width: "2%" }}>{`${
+                this.props.indexOfLastApp + index - 9
+              }.`}</td>
 
-              <td className={"align-middle table-borderless"} style={{ width: "8%" }}>
-                {this.checkIfOnSteam(app.title, app.urls.buy, <SearchResultsData appid={this.props.getIdByName(app.title)} />)}
+              <td
+                className={"align-middle table-borderless"}
+                style={{ width: "8%" }}
+              >
+                {this.checkIfOnSteam(
+                  app.title,
+                  app.urls.buy,
+                  <SearchResultsData
+                    appid={this.props.getIdByName(app.title)}
+                  />
+                )}
               </td>
 
               <td className={"align-middle"} style={{ width: "15%" }}>
                 {this.checkIfOnSteam(app.title, app.urls.buy, app.title)}
               </td>
-              <td className={"align-middle font-weight-bold font-italic"} style={{ width: "10%" }}>
+              <td
+                className={"align-middle font-weight-bold font-italic"}
+                style={{ width: "10%" }}
+              >
                 ${app.price_new}
               </td>
-              <td className={"align-middle"} style={{ width: "10%" }}>${app.price_old}</td>
-              <td className={"align-middle"} style={{ width: "5%" }}>{app.price_cut}% OFF</td>
               <td className={"align-middle"} style={{ width: "10%" }}>
-                <a className={"btn btn-success"} style={{padding: "0.25rem"}} href={app.urls.buy}>{app.shop.name}</a>
+                ${app.price_old}
+              </td>
+              <td className={"align-middle"} style={{ width: "5%" }}>
+                {app.price_cut}% OFF
+              </td>
+              <td className={"align-middle"} style={{ width: "10%" }}>
+                <a
+                  className={"btn btn-success"}
+                  style={{ padding: "0.25rem" }}
+                  href={app.urls.buy}
+                >
+                  {app.shop.name}
+                </a>
               </td>
             </tr>
           ))}
