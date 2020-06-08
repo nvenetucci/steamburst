b520bf8db917f2dae6ee2b6dfa7a78d32ac36a43
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2020-06-08 15:31:12 -0700

    intro title changed, centered on small screens now, darker

--- client/src/components/IntroTitle.js
+++ client/src/components/IntroTitle.js
@@ -14,13 +14,13 @@ class IntroTitle extends Component {
         <div
           className={"jumbotron text-center text-white bg-transparent"}
           style={{
-            width: "75%",
+            width: "100%",
             backgroundImage:
-              "linear-gradient(90deg, rgba(23,23,23,0) 0%, rgba(23,23,23,0.7) 15%, rgba(23,23,23,1) 40%, rgba(23,23,23,1) 60%, rgba(23,23,23,0.7) 85%, rgba(23,23,23,0) 100%)",
+            "linear-gradient(90deg, rgba(23,23,23,0.8) 0%, rgba(23,23,23,0.9) 15%, rgba(23,23,23,1) 40%, rgba(23,23,23,1) 60%, rgba(23,23,23,0.9) 85%, rgba(23,23,23,0.8) 100%)",
           }}
         >
-          <h1 className={"display-1"}>Steam Burst</h1>
-          <h3 className={"lead"}>
+          <h1 className={"display-1 text-center"}>Steam Burst</h1>
+          <h3 className={"lead text-center"}>
             Welcome to Steam Burst, a website dedicated to bringing you all
             sorts of information about Steam games
           </h3>
